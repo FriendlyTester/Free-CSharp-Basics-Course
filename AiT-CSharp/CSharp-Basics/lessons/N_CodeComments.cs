bcc8d23cb9674c7b2fb1ad0d4dbb18a1236e21dc
Richard Bradshaw <richardbradshaw@gmail.com>
2018-02-08 02:03:36 +0000

    added code comments lesosn

---
+++ AiT-CSharp/CSharp-Basics/lessons/N_CodeComments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CSharpBasics.lessons
+{
+    public class N_CodeComments
+    {
+        //So in VS if you type /// and press <enter> you'll get this
+
+        /// <summary>
+        /// We can write a summary of our method
+        /// </summary>
+        /// <param name="a">We can add more detail about paramter a</param>
+        /// <param name="b">We can add more detail about paramter b</param>
+        public void CodeCommentMethod(String a, int b)
+        {
+            //int c = 1; //If you press CMD or Ctrl+/ it will comment the line out/in
+        }
+
+    }
+}
